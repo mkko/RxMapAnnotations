be0daf26b24d762d3c90e25f5e01146c0fe7865b
Mikko Välimäki <mkko1373@gmail.com>
2019-01-08 19:43:00 +0200

    Initialize the project

---
+++ RxMapAnnotations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxMapAnnotations
+//
+//  Created by Mikko Välimäki on 2019-01-08.
+//  Copyright © 2019 Mikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
